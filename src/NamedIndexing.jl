064997579b8c5f67bb0e676755bc6d6c419dbd54
Mayeul d'Avezac <2745737+mdavezac@users.noreply.github.com>
2018-08-11 18:27:35 +0100

    Indexing not type stable

--- src/NamedIndexing.jl
+++ src/NamedIndexing.jl
@@ -13,6 +13,12 @@ end
 
 Base.size(AA::NamedAxisArray) = size(AA.data)
 
+is_scalar_index(::Number) = true
+is_scalar_index(::Symbol) = true
+is_scalar_index(::Char) = true
+is_scalar_index(::AbstractString) = true
+is_scalar_index(::Any) = false
+
 function Base.getindex(AA::NamedAxisArray{T, N, A, Names}, axinds::IndAx) where {T, N, A, Names, IndAx<:NamedTuple}
     fullinds = merge(
         NamedTuple{Names, NTuple{N, Colon}}(ntuple((_) -> (:), Val{N}())),
@@ -26,11 +32,14 @@ function Base.getindex(AA::NamedAxisArray{T, N, A, Names}, axinds::IndAx) where 
 
     newdata = getindex(AA.data, fullinds...)
 
-    # falls apart if you do APL-style reshaping with indexes
-    NamedAxisArray{T, N, A, Names}(newdata)
+    #  newnames = Tuple(name for (name, index) in zip(Names, fullinds)
+                     #  if is_scalar_index(index))
+    #  NamedAxisArray(newdata, newnames)
 end
 
 Base.getindex(AA::NamedAxisArray; kwargs...) = getindex(AA, kwargs.data)
+Base.getindex(AA::NamedAxisArray, i::Integer...) = getindex(AA.data, i...)
+Base.setindex!(AA::NamedAxisArray, value, i::Integer...) = setindex!(AA.data, value, i...)
 
 axisnames(AA::NamedAxisArray{T, N, A, Names}) where {T, N, A, Names} = Names
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,13 +3,34 @@ using NamedIndexing
 
 @testset "NamedIndexing" begin
 
-@testset "NamedAxisArray" begin
-    A = NamedAxisArray(rand(3, 2), (:foo, :bar))
-    @test axisnames(A) == (:foo, :bar)
+    @testset "Get index" begin
+        A = NamedAxisArray(rand(1:10, (3, 2)), (:foo, :bar))
+        @test axisnames(A) == (:foo, :bar)
 
-    Acolons = A[foo=:, bar=:]
-    @test axisnames(Acolons) == axisnames(A)
-    @test Acolons.data == A.data
-end
+        Acolons = A[foo=:, bar=:]
+        @test axisnames(Acolons) == axisnames(A)
+        @test Acolons.data == A.data
 
+        i, j = rand(1:size(A, 1)), rand(1:size(A, 2))
+        @test A[i, j] == A.data[i, j]
+
+        i = rand(1:length(A))
+        @test A[i] == A.data[i]
+    end
+
+    @testset "Set index" begin
+        A = NamedAxisArray(rand(1:10, (3, 2)), (:foo, :bar))
+
+        i, j = rand(1:size(A, 1)), rand(1:size(A, 2))
+        A[i, j] = 0
+        @test A.data[i, j] == 0
+        A[i, j] = 1
+        @test A.data[i, j] == 1
+
+        i = rand(1:length(A))
+        A[i] = 0
+        @test A.data[i] == 0
+        A[i] = 1
+        @test A.data[i] == 1
+    end
 end
